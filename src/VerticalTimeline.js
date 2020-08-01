6100aaea2ed83d28dbf0ffb9a21037e2e270af0f
Emil Boev <kycok.6o6pa@gmail.com>
2020-08-01 13:08:29 +0300

    Add VerticalTimeline component's withAltContent prop

--- src/VerticalTimeline.js
+++ src/VerticalTimeline.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const VerticalTimeline = ({ animate, className, layout, children }) => (
+const VerticalTimeline = ({ animate, className, layout, children, withAltContent }) => (
   <div
     className={classNames(className, 'vertical-timeline', {
       'vertical-timeline--animate': animate,
       'vertical-timeline--two-columns': layout === '2-columns',
       'vertical-timeline--one-column': layout === '1-column',
+      'vertical-timeline--with-alt-content': withAltContent,
     })}
   >
     {children}
@@ -22,12 +23,14 @@ VerticalTimeline.propTypes = {
   className: PropTypes.string,
   animate: PropTypes.bool,
   layout: PropTypes.oneOf(['1-column', '2-columns']),
+  withAltContent: PropTypes.bool
 };
 
 VerticalTimeline.defaultProps = {
   animate: true,
   className: '',
   layout: '2-columns',
+  withAltContent: false
 };
 
 export default VerticalTimeline;
